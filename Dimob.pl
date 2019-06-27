7497c971d6a68536e2ba73c5abd9db7dbdb3530a
jfsanchez <jfbioinformatics@gmail.com>
2019-06-27 10:16:43 +0200

    Add logging messages; GI min_length as variable

--- Dimob.pl
+++ Dimob.pl
@@ -55,15 +55,52 @@ MAIN: {
     #my $logger_conf = "$RealBin/logger.conf";
 
     # usage help
-    my $usage = "Usage:\n./Dimob.pl <genome.gbk> <outputfile.txt> cutoff_genes[int]\nExample:\n./Dimob.pl example/NC_003210.gbk NC_003210_GIs.txt 8\n";
+    my $usage = "Usage:\n./Dimob.pl <genome.gbk> <outputfile.txt> [cutoff_dinuc_bias == int] [min_length == int] \nExample:\n./Dimob.pl example/NC_003210.gbk NC_003210_GIs.txt 8 8000\n";
 
-    my ($inputfile, $outputfile, $cutoff_genes) = @ARGV;
+    my ($inputfile, $outputfile, $cutoff_dinuc_bias, $min_length) = @ARGV;
 
     # Check that input file and output file are specified or die and print help message
     unless(defined($inputfile) && defined($outputfile)){
         print $usage;
         exit;
     }
+    
+    ## min_length
+	if (!$min_length) { $min_length = 8000; }
+    
+    # Create a dimob object
+    my $dimob_obj = Dimob->new(
+        {
+        cfg_file => $cfname,
+        bindir => $RealBin,
+        workdir => $cwd,
+        MIN_GI_SIZE => $min_length,
+        extended_ids => 1
+        }
+    );
+
+	# Recover the config from file, initialized during creation dimob_obj
+    my $cfg = Dimob::Config->config;
+    $cfg->{logger_conf} = $RealBin."/".$cfg->{logger_conf};
+    $cfg->{hmmer_db} = "$RealBin/".$cfg->{hmmer_db};
+
+    # Check that the logger exists and initializes it
+    #print $cfg->{logger_conf};
+    if($cfg->{logger_conf} && ( -r $cfg->{logger_conf})) {
+        Log::Log4perl::init($cfg->{logger_conf});
+        $logger = Log::Log4perl->get_logger;
+        #$logger->debug("Logging initialized");
+    }
+
+    $logger->debug("IslandPath-DIMOB initialized");
+	
+	## min_length
+	if (!$min_length) { 
+		$min_length = 8000;
+	    $logger->debug("Use default min_length: 8000 bp");
+	} else {
+	    $logger->debug("Use min_length: $min_length");
+	}
 
     # Transform relative path to absolute path and 
     # check that input file is readable
@@ -73,35 +110,12 @@ MAIN: {
         exit;
     }
 
-    # Create a dimob object
-    my $dimob_obj = Dimob->new(
-        {cfg_file => $cfname,
-            bindir => $RealBin,
-            workdir => $cwd,
-            MIN_GI_SIZE => 2000,
-            extended_ids => 1
-        }
-    );
-    
-    # Recover the config from file, initialized during creation dimob_obj
-    my $cfg = Dimob::Config->config;
-    $cfg->{logger_conf} = "$RealBin/" . $cfg->{logger_conf};
-    $cfg->{hmmer_db} = "$RealBin/" . $cfg->{hmmer_db};
-
-    # Check that the logger exists and initializes it
-    #print $cfg->{logger_conf};
-    if($cfg->{logger_conf} && ( -r $cfg->{logger_conf})) {
-        Log::Log4perl::init($cfg->{logger_conf});
-        $logger = Log::Log4perl->get_logger;
-        $logger->debug("Logging initialized");
-    }
-
-	## check if $cutoff_genes provided
-	if (!$cutoff_genes) {
-		$cutoff_genes = 8;
-        $logger->debug("Use cutoff_genes default: 8");
+    ## check if $cutoff_genes provided
+	if (!$cutoff_dinuc_bias) {
+		$cutoff_dinuc_bias = 8;
+        $logger->debug("Use cutoff_dinuc_bias default: 8");
 	} else {
-        $logger->debug("Use cutoff_genes provided: ".$cutoff_genes);
+        $logger->debug("Use cutoff_dinuc_bias provided: ".$cutoff_dinuc_bias);
 	}
 	
     # Create a tmp directory to store intermediate results, copy the input file to the tmp
@@ -139,18 +153,28 @@ MAIN: {
     # Runs IslandPath-DIMOB on the genome files
 
     $logger->info("Running IslandPath-DIMOB");
-    my @islands = $dimob_obj->run_dimob($inputfile, $outputfile, $cutoff_genes);
+    my @islands = $dimob_obj->run_dimob($inputfile, $outputfile, $cutoff_dinuc_bias);
 
     $logger->info("Printing results");
 
     my $i = 1;
-    open my $fhgd, '>', $outputfile or die "Cannot open output.txt: $!";
+    open my $fhgd, '>', $outputfile or die "Cannot open $outputfile: $!";
+	my $discard_file = $outputfile."_discard.txt";
+	open DISCARD, '>', $discard_file or die "Cannot open $discard_file: $!";
+	print DISCARD "seq\tstart\tend\tlength\n";
     foreach my $island (@islands) {
         my $start = $island->[0];
         my $end = $island->[1];
         my $seq = $island->[2];
-        print $fhgd "GI_$i\t$seq\t$start\t$end\n";
-        $i++;
+        
+        ## discard if smaller than min length set
+        my $diff = $end - $start;        
+        if ($diff < $min_length) {
+        	print DISCARD "$seq\t$start\t$end\t$diff\n";
+        } else {
+	        print $fhgd "GI_$i\t$seq\t$start\t$end\n";
+	        $i++;
+        }
     }
     close $fhgd;
 	
@@ -162,3 +186,14 @@ MAIN: {
         $logger->error("Can't remove $tmp_path: $!");
     }
 }
+
+## 
+## New implementations
+## 
+## Add multicontig function
+## Fix smartmacth experimental warning message
+## Fix dinuc bias loop iteration bug
+## Use cutoff_dinuc_bias as a variable
+## Output csv information for dinucleotide bias.
+## Provide additional information in output GI information
+## 
